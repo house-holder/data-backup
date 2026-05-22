e7e0edda5482fa29a55c24cc7a899f9f19da516c
kh <kylemhouseholder@gmail.com>
2026-05-22 08:17:20 -0500

    chore: ignored testing files

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-import sys
 from hashlib import sha256
 
-def main():
-    for arg in sys.argv[1:]:
-        print(getFileChecksum(arg))
-
-def getFileChecksum(filePath: str) -> str:
-    h = sha256()
+def get256String(filePath: str) -> str:
+    fileB: bytes
     with open(filePath, "rb") as f:
-        for chunk in iter(lambda: f.read(8192), b""):
-            h.update(chunk)
-    return h.hexdigest()
+        fileB = f.read()
+    return sha256(fileB).hexdigest()
+
+def main():
+    testFiles: str = "./.test-files/"
+    print(testFiles) # NOTE: silences unused warning
 
 if __name__ == "__main__":
     main()
