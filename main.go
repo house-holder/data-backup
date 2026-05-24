9b07a36525088fc5f6d8ee9f159e5fdd74ddfbc1
kh <kylemhouseholder@gmail.com>
2026-05-23 23:07:50 -0500

    feat: add working core update functions
    Refactor to clean up main and rescope responsibility to the registry
    struct. Add remote + local filehandling. Add additional fields to backup
    registryfile.

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"os/exec"
 	"time"
@@ -13,30 +14,116 @@ import (
 	"github.com/BurntSushi/toml"
 )
 
-type DBFile struct {
-	Size int    `json:"size_bytes"`
-	Hash string `json:"hash"`
+const maxDBEntries = 10
+
+type BackupEntry struct {
+	SizeBytes  int    `json:"size_bytes"`
+	Epoch      int64  `json:"timestamp"`
+	LocalPath  string `json:"local_path"`
+	RemotePath string `json:"remote_path"`
+	Hash       string `json:"hash"`
 }
 
 type Registry struct {
-	LastBackup int64             `json:"last_backup"`
-	BytesUsed  int               `json:"bytes_used"`
-	Backups    map[string]DBFile `json:"backups"`
+	LastBackup int64                  `json:"last_backup"`
+	TotalBytes int                    `json:"total_bytes"`
+	Backups    map[string]BackupEntry `json:"backups"`
 }
 
-type DBCfg struct {
+func (r *Registry) update() {
+	if len(r.Backups) > maxDBEntries {
+		filename := ""
+		var pruneEpoch int64 = math.MaxInt64
+
+		for name, entry := range r.Backups {
+			if entry.Epoch < pruneEpoch {
+				pruneEpoch = entry.Epoch
+				filename = name
+			}
+		}
+		r.deleteBackup(filename, r.Backups[filename].RemotePath)
+	}
+	r.updateBytes()
+}
+
+func (r *Registry) updateBytes() {
+	r.TotalBytes = 0
+	for _, file := range r.Backups {
+		r.TotalBytes += file.SizeBytes
+	}
+}
+
+func (r *Registry) createBackup(name string, db *DB, remote string) error {
+	tm := time.Now()
+	entry, err := getFileInfo(db.Source)
+	if err != nil {
+		log.Fatalf("failed to get file info: %v", err)
+	}
+	if !shouldBackup(entry, r) {
+		return nil
+	}
+
+	filename := datestampFilename(tm, name)
+
+	entry.Epoch = tm.Unix()
+	entry.LocalPath = fmt.Sprintf("%s%s", db.Stage, filename)
+	entry.RemotePath = fmt.Sprintf("%s/%s", remote, filename)
+
+	r.LastBackup = tm.Unix()
+	r.Backups[filename] = *entry
+
+	subCmdLocal := fmt.Sprintf(".backup '%s'", entry.LocalPath)
+	cmdLocal := exec.Command("sqlite3", db.Source, subCmdLocal)
+	out, err := cmdLocal.Output()
+	if out != nil {
+		fmt.Println(string(out))
+	}
+	if err != nil {
+		return err
+	}
+
+	cmdRemote := exec.Command("rclone", "copy", entry.LocalPath, remote)
+	out, err = cmdRemote.Output()
+	if out != nil {
+		fmt.Println(string(out))
+	}
+	if err != nil {
+		return err
+	}
+	r.update()
+	return nil
+}
+
+func (r *Registry) deleteBackup(filename string, remote string) error {
+	err := os.Remove(r.Backups[filename].LocalPath)
+	if err != nil {
+		return fmt.Errorf("failed to remove local file: %v", err)
+	}
+	cmd := exec.Command("rclone", "deletefile", remote)
+	out, err := cmd.Output()
+	if out != nil {
+		fmt.Println(string(out))
+	}
+	if err != nil {
+		return err
+	}
+	delete(r.Backups, filename)
+	return nil
+}
+
+type DB struct {
 	Source string `toml:"source"`
 	Stage  string `toml:"stage"`
 }
 
-type Config struct {
-	RegistryPath string           `toml:"registry_path"`
-	RemotePath   string           `toml:"remote_path"`
-	Databases    map[string]DBCfg `toml:"databases"`
+type DatabaseConfig struct {
+	RegistryPath string        `toml:"registry_path"`
+	RemotePath   string        `toml:"remote_path"`
+	Databases    map[string]DB `toml:"databases"`
 }
 
-func saveRegistry(r map[string]Registry, regPath string) error {
-	data, err := json.MarshalIndent(r, "", "\t")
+func saveState(r map[string]*Registry, regPath string) error {
+	data, err := json.MarshalIndent(r, "", "  ")
 	if err != nil {
 		return fmt.Errorf("failed to marshal registry: %w", err)
 	}
@@ -47,7 +134,7 @@ func saveRegistry(r map[string]Registry, regPath string) error {
 	return nil
 }
 
-func loadRegistry(path string, r *map[string]Registry) error {
+func loadState(path string, r *map[string]*Registry) error {
 	jsonData, err := os.ReadFile(path)
 	if os.IsNotExist(err) {
 		return nil
@@ -64,12 +151,12 @@ func loadRegistry(path string, r *map[string]Registry) error {
 	return nil
 }
 
-func loadConfig(path string, c *Config) error {
+func loadConfig(path string, dbCfg *DatabaseConfig) error {
 	data, err := os.ReadFile(path)
 	if err != nil {
 		return fmt.Errorf("failed to read config: %w", err)
 	}
-	err = toml.Unmarshal(data, c)
+	err = toml.Unmarshal(data, dbCfg)
 	if err != nil {
 		return fmt.Errorf("failed to unmarshal config: %w", err)
 	}
@@ -82,15 +169,17 @@ func getSHA256(data []byte) string {
 	return hex.EncodeToString(h.Sum(nil))
 }
 
-func getFileInfo(path string) (*DBFile, error) {
-	var f DBFile
+func getFileInfo(path string) (*BackupEntry, error) {
+	var e BackupEntry
+
 	bytes, err := os.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
-	f.Hash = getSHA256(bytes)
-	f.Size = len(bytes)
-	return &f, nil
+	e.Hash = getSHA256(bytes)
+	e.SizeBytes = len(bytes)
+
+	return &e, nil
 }
 
 func getConfig() string {
@@ -101,65 +190,55 @@ func getConfig() string {
 	return "/etc/data-backup/config.toml"
 }
 
-func loadInit() (*Config, *map[string]Registry) {
-	var cfg Config
-	var regMap map[string]Registry
+func loadInit() (*DatabaseConfig, *map[string]*Registry) {
+	var dbCfg DatabaseConfig
+	regMap := map[string]*Registry{}
 
-	err := loadConfig(getConfig(), &cfg)
+	err := loadConfig(getConfig(), &dbCfg)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = loadRegistry(cfg.RegistryPath, &regMap)
+	err = loadState(dbCfg.RegistryPath, &regMap)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return &cfg, &regMap
+	return &dbCfg, &regMap
 }
 
-func shouldBackup(f *DBFile, r *Registry) bool {
+func shouldBackup(e *BackupEntry, r *Registry) bool {
 	for _, backup := range r.Backups { //nil backups = fallthrough
-		if backup.Hash == f.Hash {
+		if backup.Hash == e.Hash {
+			fmt.Println("found matching hash")
 			return false
 		}
 	}
 	return true
 }
 
-func fmtFilename(t time.Time, dbName string) string {
+func datestampFilename(t time.Time, dbName string) string {
 	timeStr := fmt.Sprintf("%d-%.2d-%.2d", t.Year(), int(t.Month()), t.Day())
 	return fmt.Sprintf("%s-%s.db", timeStr, dbName)
 }
 
-func backupDatabase(dbName string, cfg *DBCfg, reg *Registry) error {
-	t := time.Now()
-	reg.LastBackup = t.Unix()
-	filePath := fmt.Sprintf("%s/%s", cfg.Stage, fmtFilename(t, dbName))
-
-	subcommand := fmt.Sprintf(".backup '%s'", filePath)
-	cmd := exec.Command("sqlite3", cfg.Source, subcommand)
-	out, err := cmd.Output()
-	if err != nil {
-		return err
-	}
-
-	fmt.Println(string(out))
-	return nil
-}
-
-func computeBackupSize(reg *Registry) {}
-
 func main() {
 	cfg, registryMap := loadInit()
 
-	for dbName, dbCfg := range cfg.Databases {
-		file, err := getFileInfo(dbCfg.Source)
-		if err != nil {
-			log.Fatalf("failed to get file info: %v", err)
-		}
+	for dbName, db := range cfg.Databases {
+		remote := cfg.RemotePath + dbName
+
 		registry, ok := (*registryMap)[dbName]
-		if !ok || shouldBackup(file, &registry) {
-			backupDatabase(dbName, &dbCfg, &registry)
+		if !ok {
+			newRegistry := Registry{Backups: map[string]BackupEntry{}}
+			(*registryMap)[dbName] = &newRegistry
+			registry = &newRegistry
 		}
+
+		registry.createBackup(dbName, &db, remote)
+	}
+
+	err := saveState(*registryMap, cfg.RegistryPath)
+	if err != nil {
+		log.Fatalf("problem saving application state: %v", err)
 	}
 }
